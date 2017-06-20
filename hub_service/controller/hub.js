60c7e74ea98bf3b2597963a56f811e55c610caa6
g wilken <greg@gwilken.com>
2017-06-20 00:29:55 -0700

    back to ws websockets

--- hub_service/controller/hub.js
+++ hub_service/controller/hub.js
@@ -1,4 +1,5 @@
 
+const WebSocket = require('ws');
 const mongo = require("../model/mongo.js");
 const url = require('url');
 const net = require("net");
@@ -8,39 +9,41 @@ const gps = require("./gps");
 const motor = require("./motor");
 
 
+var sensor = {};
+var ws = null;
+const realTimeInterval = 3000;
 
 
+ws = new WebSocket('ws://www.rednightsky.com:8080');
 
+ws.on('open', function open() {
+  console.log('Websocket connection open.');
+});
 
+ws.on('message', function incoming(data) {
+  console.log(data);
+});
 
-module.exports = function(app) {
+ws.on('error', function(err) {
+  console.log('error at web socket:', err);
+});
 
-  const server = require('http').createServer();
-  const io = require('socket.io')(server);
+ws.on('close', () => {
+  console.log('Websocket disconnected.');
+});
 
-  server.listen(3000);
+ws.onclose = function() {
+  console.log('Connection to external server closed.');
 
-  var socket = io();
+  setTimeout(function() {
+    console.log('Attempting to reconnect to external server...');
+    connect();
+  }, 5000);
+};
 
-  socket.connect('http://www.rednightsky.com:3000');
-
-  io.on('connect', (socket) => {
-
-    console.log('connected.');
-
-  });
-
-
-
-  var remoteServer = null;
-  const realTimeInterval = 3000;
-
-  var sensor = {};
-
-  //io.on('connection', function(){ /* … */ });
-
-  server.listen(3000);
-
+var connect = function () {
+  ws = new WebSocket('ws://www.rednightsky.com:8080');
+};
 
   // wss.on('connection', (ws) => {
   //   console.log('Websocket client connected...');
@@ -49,39 +52,13 @@ module.exports = function(app) {
   //     console.log('error at web socket server:', err);
   //   });
   //
-  //   ws.on('close', () => {
-  //     console.log('Client disconnected');
-  //   });
+
   //
-  //   ws.on('error', function(err) {
-  //     console.log('error at web socket:', err);
-  //   });
+
   // });
 
 
-  var connectExternalServer = function () {
 
-    remoteServer = new WebSocket( url.format('ws://50.116.5.92') );
-
-    remoteServer.on('open', function() {
-      console.log('Connected to external server.');
-    });
-
-    remoteServer.on('error', function(err) {
-      console.log('Error on external server socket. Is it up?', err);
-
-    });
-
-    remoteServer.onclose = function() {
-      console.log('Connection to external server closed.');
-
-      setTimeout(function() {
-        console.log('Attempting to reconnect to external server...');
-        connectExternalServer();
-      }, 5000);
-
-    };
-  };
 
   //
   // setInterval(function() {
@@ -102,26 +79,22 @@ module.exports = function(app) {
 
 
   setInterval(function() {
-    if (remoteServer.readyState === WebSocket.OPEN) {
-      remoteServer.send( JSON.stringify( gps ) );
-      remoteServer.send( JSON.stringify( motor ) );
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send( JSON.stringify( gps ) );
+      ws.send( JSON.stringify( motor ) );
     };
   }, 1000);
+
 
   const sensorServer = net.createServer(function(socket) {
     socket.on("data", function(data) {
       sensor = JSON.parse(data);
 
-      if (remoteServer.readyState === WebSocket.OPEN) {
-        remoteServer.send( JSON.stringify( sensor ) );
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send( JSON.stringify( sensor ) );
       };
 
     })
   });
 
   sensorServer.listen(3215, '192.168.10.1');
-
-
-  connectExternalServer();
-
-}
