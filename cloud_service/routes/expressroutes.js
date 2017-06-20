f2d552ec958b7ae42e7cc66e54b3f23f9de78830
g wilken <greg@gwilken.com>
2017-06-20 15:13:46 -0700

    react work

--- cloud_service/routes/expressroutes.js
+++ cloud_service/routes/expressroutes.js
@@ -9,7 +9,7 @@ router.get('/history/:name/:field', function(req, res) {
 
   console.log('history route hit', req.params);
 
-  mongo.collection.find({}).toArray( function(err, doc) {
+  mongo.collection.find({}, { _id: 0, telemetry: { "Solar Controller Monitor" : {   "current" : 1}}} ).toArray( function(err, doc) {
 
     res.json(doc);
   })
