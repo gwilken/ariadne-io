200ec41a86cef3ff3bca752a87b91e3ee49795f7
g wilken <greg@gwilken.com>
2017-06-20 18:44:19 -0700

    react work

--- cloud_service/model/mongo.js
+++ cloud_service/model/mongo.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://localhost:27017/ariadneIo';
+const url = 'mongodb://localhost:27017/ariadneIO';
 
 var mongo = {
 
