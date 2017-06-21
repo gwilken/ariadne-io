5ed37c3dd5ca9ac8db3a58368be7d279646dfe76
g wilken <greg@gwilken.com>
2017-06-21 15:42:59 -0700

    mongo route, projection working

--- cloud_service/app/components/Solar.js
+++ cloud_service/app/components/Solar.js
@@ -200,7 +200,7 @@ class Solar extends React.Component {
 
             <div className="titlebar">
               <div className="title"> Volts</div>
-              <div className="rtData"> {this.props.data.loadvoltage 'v'} </div>
+              <div className="rtData"> {this.props.data.loadvoltage + 'v'} </div>
             </div>
 
           </div>
