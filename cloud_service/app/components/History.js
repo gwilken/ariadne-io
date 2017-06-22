abd844e4e26fb2a25335c2f9265f1f42488ec322
g wilken <greg@gwilken.com>
2017-06-22 13:29:05 -0700

    building react compononents

--- cloud_service/app/components/History.js
+++ cloud_service/app/components/History.js
@@ -13,7 +13,7 @@ class History extends React.Component {
 
   componentDidMount() {
 
-    fetch('/datapoint/test/test/5');
+    fetch('/datapoint/test/test/60');
 
       // .then((res) => res.json())
       //   .then(function(objs) {
