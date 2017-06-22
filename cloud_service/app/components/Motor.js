bc81fc505eb1a3ee5052ffa4355c4a57ef4b3478
g wilken <greg@gwilken.com>
2017-06-21 21:21:02 -0700

    building react compononents

--- cloud_service/app/components/Motor.js
+++ cloud_service/app/components/Motor.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Bar} from 'react-chartjs-2';
+import {Doughnut} from 'react-chartjs-2';
+
 
 class Motor extends React.Component {
 
@@ -130,6 +132,35 @@ class Motor extends React.Component {
         }
       }
 
+      var doughnutData = {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      };
+
+    var DoughnutOptions = {
+          rotation: 1 * Math.PI,
+          circumference: 1 * Math.PI
+      };
 
     content = (
 
@@ -159,7 +190,11 @@ class Motor extends React.Component {
 
 
         <div className="graphContainer">
-
+          <Doughnut data={doughnutData}
+            options={doughnutOptions}
+              width={800}
+              height={140}
+          />
         </div>
 
           {/* <div className="graphContainer">
--- cloud_service/public/bundle.js
+++ cloud_service/public/bundle.js
@@ -53231,6 +53231,22 @@
 	          }
 	        };
 
+	        var doughnutData = {
+	          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+	          datasets: [{
+	            label: '# of Votes',
+	            data: [12, 19, 3, 5, 2, 3],
+	            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
+	            borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+	            borderWidth: 1
+	          }]
+	        };
+
+	        var DoughnutOptions = {
+	          rotation: 1 * Math.PI,
+	          circumference: 1 * Math.PI
+	        };
+
 	        content = _react2.default.createElement(
 	          'div',
 	          null,
@@ -53283,7 +53299,15 @@
 	                )
 	              )
 	            ),
-	            _react2.default.createElement('div', { className: 'graphContainer' })
+	            _react2.default.createElement(
+	              'div',
+	              { className: 'graphContainer' },
+	              _react2.default.createElement(_reactChartjs.Doughnut, { data: doughnutData,
+	                options: doughnutOptions,
+	                width: 800,
+	                height: 140
+	              })
+	            )
 	          )
 	        );
 	      }
