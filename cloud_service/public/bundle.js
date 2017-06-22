c67705f1b287225e4fc8e6b4e79ae9f9e79bf9c3
g wilken <greg@gwilken.com>
2017-06-21 21:32:18 -0700

    building react compononents

--- cloud_service/public/bundle.js
+++ cloud_service/public/bundle.js
@@ -53232,12 +53232,11 @@
 	        };
 
 	        var doughnutData = {
-	          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+	          labels: ["Red", "darker red"],
 	          datasets: [{
 	            label: '# of Votes',
-	            data: [12, 19, 3, 5, 2, 3],
-	            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-	            borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+	            data: [12, 1],
+	            backgroundColor: ['firebrick', 'rgb(107, 20, 20)'],
 	            borderWidth: 1
 	          }]
 	        };
