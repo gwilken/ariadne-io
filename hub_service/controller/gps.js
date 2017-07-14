c8bdac7398403da5acf964e4bee09b121d50afee
g wilken <greg@gwilken.com>
2017-07-14 00:19:25 -0700

    gps data json update

--- hub_service/controller/gps.js
+++ hub_service/controller/gps.js
@@ -2,12 +2,29 @@ const net = require("net");
 const nmea = require("nmea-simple");
 
 var gps = {
-  family: 'navigation',
-  latitude: null,
-  longitude: null,
-  speed: null,
-  tmg: null,
-  siv: null
+  family: 'gps',
+  displayName: "Geospatial",
+  data: [{
+      sensor: 'position',
+      displayName: 'Position',
+      data: [null, null]
+    },
+    {
+      sensor: 'speed',
+      displayName: 'Speed',
+      data: null
+    },
+    {
+      sensor: 'tmg',
+      displayName: 'Track Made Good',
+      data: null
+    },
+    {
+      sensor: 'siv',
+      displayName: "Satellites in View",
+      data: null
+    }
+  ]
 }
 
 const gpsSocket = net.connect({port: 10110}, () => {
@@ -19,21 +36,18 @@ gpsSocket.on("data", function(data) {
 
     var packet = nmea.parseNmeaSentence(data.toString());
 
-    if (packet.sentenceId === "RMC" && packet.status === "valid") {
-
-      gps.speed = packet.speedKnots;
-      gps.tmg = packet.trackTrue;  //Track Made Good
+    if (packet.sentenceId === "GGA" && packet.fixType !== "none") {
+      gps.data[0].data[0] = packet.latitude;
+      gps.data[0].data[1] = packet.longitude;
     };
 
-    if (packet.sentenceId === "GGA" && packet.fixType !== "none") {
-
-      gps.latitude = packet.latitude;
-      gps.longitude = packet.longitude;
+    if (packet.sentenceId === "RMC" && packet.status === "valid") {
+      gps.data[1].data = packet.speedKnots;
+      gps.data[2].data = packet.trackTrue;  //Track Made Good
     };
 
     if (packet.sentenceId === "GSA") {
-
-      gps.siv = packet.satellites.length;   //Satellites In View
+      gps.data[3].data = packet.satellites.length;   //Satellites In View
     };
 
   } catch (error) { }   //catch AIS messages that would otherwise break the parser.
@@ -41,13 +55,11 @@ gpsSocket.on("data", function(data) {
 });
 
 gpsSocket.on("close", function(data) {
-  gps.latitude = "Not Availabe";
-  gps.longitude = "Not Availabe";
-  gps.speed = 0;
-  gps.lastAverage = 0;
-  gps.buffer = [],
-  gps.tmg = "Not Availabe";
-  gps.siv = "Not Availabe";
+  gps.data[0].data[0] = "Not Availabe";
+  gps.data[0].data[1] = "Not Availabe";
+  gps.data[1].data = 0;
+  gps.data[2].data = "Not Availabe";
+  gps.data[3].data = "Not Availabe";
 });
 
 gpsSocket.on("error", function(err) {
