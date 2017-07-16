6c933ee2dcae8541fa570e0479376fabe4d34381
g wilken <greg@gwilken.com>
2017-07-15 19:42:10 -0700

    hub mongo connection

--- hub_service/controller/wifisensors.js
+++ hub_service/controller/wifisensors.js
@@ -6,6 +6,7 @@ const sensorServer = net.createServer(function(socket) {
   socket.on("data", function(data) {
     try {
       packet = JSON.parse(data);
+      console.log(packet);
     } catch(err) {
       console.log('error at wireless sensor', err);
     }
