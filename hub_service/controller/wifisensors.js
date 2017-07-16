36d945e6a754a7d1c2f42cbd73b6eb2980f2b756
g wilken <greg@gwilken.com>
2017-07-15 21:32:34 -0700

    hub mongo connection

--- hub_service/controller/wifisensors.js
+++ hub_service/controller/wifisensors.js
@@ -4,27 +4,28 @@ const mongo = require("../model/mongo.js");
 
 var packet = {};
 var sensor = {};
-var dataArr = [];
 var count = 0;
 
 const sensorServer = net.createServer(function(socket) {
   socket.on("data", function(data) {
     try {
       packet = JSON.parse(data);
+
       Object.assign(sensor, packet);
-      sensor._id = new ObjectID();
-      dataArr.push(sensor);
+
     } catch(err) {
       console.log('error at wireless sensor', err);
     }
 
-    if(dataArr.length > 70) {
-    //  sensor._id = new ObjectID();
-      mongo.collection.insertMany(dataArr, function(err) {
+    count++;
+
+    if(count === 50) {
+      sensor._id = new ObjectID();
+      mongo.collection.insert(sensor, function(err) {
         if(err) console.log('error at sensor mongo insert', err);
         console.log('sensor data inserted');
       })
-      dataArr = [];
+      count = 0;
     }
 
   })
