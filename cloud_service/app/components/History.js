747ee7eb6821c358fbefc4ff1aefb4ab0cfd9e8b
g wilken <greg@gwilken.com>
2017-07-17 14:58:27 -0700

    refactoring frontend react components

--- cloud_service/app/components/History.js
+++ cloud_service/app/components/History.js
@@ -21,7 +21,7 @@ class History extends React.Component {
   componentDidMount() {
     fetch(`/telemetry/${this.state.family}/${this.state.displayName}/${this.state.time}`)
       .then((res) => res.json())
-        .then(function(docs) {
+        .then((docs) => {
             this.didLoad(docs);
         });
   };
