2c7c6c1dcf544826a30868751f2484d788ee878a
g wilken <greg@gwilken.com>
2017-07-17 15:44:26 -0700

    refactoring frontend react components

--- cloud_service/app/components/History.js
+++ cloud_service/app/components/History.js
@@ -12,7 +12,7 @@ class History extends React.Component {
       displayName: this.props.view.displayName,
       color: this.props.view.color,
       time: 30,
-      docs: []
+      data: []
     }
 
     this.didLoad = this.didLoad.bind(this);
@@ -27,18 +27,90 @@ class History extends React.Component {
   };
 
   didLoad(docs) {
-    this.setState({docs: docs});
+    this.setState({data: docs});
   }
 
   render() {
-    console.log('state:',this.state);
 
+    var options = {
+      layout: {
+        padding: {
+          left: 15,
+          right: 3,
+        },
+      },
+      tooltips: {
+        enabled: false,
+      },
+      legend: {
+        display: false,
+        position: 'top',
+      },
+      // animation: {
+      //   // duration: 400,
+      //   easing: 'linear'
+      // },
+      // maintainAspectRatio: false,
+      scales: {
+        yAxes: [{
+          position: 'right',
+          ticks: {
+            mirror: false,
+           },
+          }],
+        xAxes: [{
+          gridLines: {
+            display: false,
+            drawTicks: false,
+          },
+          scaleLabel: {
+            display: true,
+          },
+          ticks: {
+            display: false,
+          },
+        },
+        ],
+      },
+    };
+
+    if(this.state.data.length > 0) {
+      var data = {
+        labels: this.state.data,
+        datasets: [
+            {
+              fill: true,
+              backgroundColor: this.state.color,
+              borderWidth: 2,
+              lineTension: 0.1,
+              pointRadius: 0,
+              data: this.state.data
+            }
+         ]
+      }
+
+      chart = (
+        <Line data={data}
+            options={options}
+            width={800}
+            height={465}
+        />
+      )
+    }
     return(
-      <div>
+    
 
-        <h1>history</h1>
+               <div className="historyContainer">
 
-      </div>
+                  <div className="historyGraph">
+
+                    {chart}
+
+                  </div>
+
+                </div>
+
+
     )
   }
 
