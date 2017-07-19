3f946944001b1e8493bd4b50b5979b8a97389015
g wilken <greg@gwilken.com>
2017-07-18 17:25:38 -0700

    refactoring frontend react components

--- cloud_service/app/components/History.js
+++ cloud_service/app/components/History.js
@@ -15,7 +15,7 @@ class History extends React.Component {
       color: this.props.view.color,
       unit: this.props.view.unit,
       time: 180,
-      chartHeight: Math.floor(window.innerHeight * .60),
+      chartHeight: Math.floor(window.innerHeight * .58),
       data: [],
       average: null,
       high: null,
