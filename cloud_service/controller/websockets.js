bffd1968818c1ed56a14d78aa275490554b8e4e6
g wilken <greg@gwilken.com>
2017-07-27 15:30:04 -0700

    spa done

--- cloud_service/controller/websockets.js
+++ cloud_service/controller/websockets.js
@@ -1,16 +1,16 @@
 const WebSocket = require('ws');
 const mongo = require("../model/mongo.js");
 
-//const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 });
 //below for local testing
-var ws = new WebSocket('ws://www.rednightsky.com:8080');
+//var ws = new WebSocket('ws://www.rednightsky.com:8080');
 
 
 var telemetry = [];
 var count = 0;
 var data;
 
-//wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(packet) {
 
@@ -20,11 +20,11 @@ var data;
       console.log('error at parse incoming json', err);
     }
 
-    // wss.clients.forEach(function each(client) {
-    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-    //     client.send(packet);
-    //   }
-    // });
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(packet);
+      }
+    });
 
   });
 
@@ -43,4 +43,4 @@ var data;
     }
   }, 120000)
 
-//});
+});
