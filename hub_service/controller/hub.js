c39c72bba48d171b853ec9ec4073b2bcff098f0c
g wilken <greg@gwilken.com>
2017-07-27 17:18:40 -0700

    hub service to SPA

--- hub_service/controller/hub.js
+++ hub_service/controller/hub.js
@@ -10,8 +10,7 @@ const telemetry = require("./wifisensors");
 
 module.exports = function(app) {
 
-  var ws;
-  //const server = http.createServer(app);
+  var internetServer;
   const wss = new WebSocket.Server({port: 8080});
 
   wss.on('connection', function connection(ws) {
@@ -25,34 +24,29 @@ module.exports = function(app) {
         console.log('error at parse incoming json', err);
       }
 
-      wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(packet);
-        }
-      });
     });
   });
 
   var connect = function () {
-    ws = new WebSocket('ws://www.rednightsky.com:8080');
+    internetServer = new WebSocket('ws://www.rednightsky.com:8080');
 
-    ws.on('open', function open() {
+    internetServer.on('open', function open() {
       console.log('Websocket connection open.');
     });
 
-    ws.on('message', function incoming(data) {
+    internetServer.on('message', function incoming(data) {
       console.log(data);
     });
 
-    ws.on('error', function(err) {
+    internetServer.on('error', function(err) {
       console.log('error at web socket.');
     });
 
-    ws.on('close', () => {
+    internetServer.on('close', () => {
       console.log('Websocket disconnected.');
     });
 
-    ws.onclose = function() {
+    internetServer.onclose = function() {
       console.log('Connection to external server closed. Attempting reconnect in 5 sec');
       setTimeout(connect, 5000);
     };
@@ -75,9 +69,16 @@ module.exports = function(app) {
         telemetry.splice(list.indexOf(gps.displayName), 1, gps);
       }
 
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.send( JSON.stringify( telemetry ) );
+    if (internetServer.readyState === WebSocket.OPEN) {
+      internetServer.send( JSON.stringify( telemetry ) );
     };
+
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(telemetry);
+      }
+    });
+
   }, 1000);
 
 
