ad5bd4a0223a36c28bc4afd5bd705cb076084de2
g wilken <greg@gwilken.com>
2017-08-24 18:03:55 -0700

    added updating mini history

--- hub_service/app/components/Motor.js
+++ hub_service/app/components/Motor.js
@@ -24,15 +24,15 @@ class Motor extends React.Component {
       var motorRPM = motorData[0].data.filter((elem) => {return elem.sensor === "rpm"});
     }
 
-    var battery1 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 1'});
-    var battery2 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 2'});
-    var battery3 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 3'});
-    var battery4 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 4'});
-
-    if(battery1.length > 0) var battery1_volts = battery1[0].data[0].data.toFixed(2);
-    if(battery2.length > 0) var battery2_volts = battery2[0].data[0].data.toFixed(2);
-    if(battery3.length > 0) var battery3_volts = battery3[0].data[0].data.toFixed(2);
-    if(battery4.length > 0) var battery4_volts = battery4[0].data[0].data.toFixed(2);
+    // var battery1 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 1'});
+    // var battery2 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 2'});
+    // var battery3 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 3'});
+    // var battery4 = this.props.data.filter((elem) => {return elem.displayName === 'Battery 4'});
+    //
+    // if(battery1.length > 0) var battery1_volts = battery1[0].data[0].data.toFixed(2);
+    // if(battery2.length > 0) var battery2_volts = battery2[0].data[0].data.toFixed(2);
+    // if(battery3.length > 0) var battery3_volts = battery3[0].data[0].data.toFixed(2);
+    // if(battery4.length > 0) var battery4_volts = battery4[0].data[0].data.toFixed(2);
 
 
     return (
