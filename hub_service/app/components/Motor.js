5a859da4b08e6721aa777b54bcbf8a6809e18b03
g wilken <greg@gwilken.com>
2017-08-25 14:38:46 -0700

    added updating mini history

--- hub_service/app/components/Motor.js
+++ hub_service/app/components/Motor.js
@@ -31,12 +31,14 @@ class Motor extends React.Component {
 
       var batt1Data = this.props.data.filter((elem) => {return elem.displayName === "Battery 1"});
 
-    if(batt1Data.length > 0) {
+      console.log(batt1Data);
 
-      console.log(batt1Data[0].data);
-      console.log(this.props.data[0].family);
-      console.log(batt1Data[0].displayName);
-      console.log(batt1Data[0].unit);
+  //  if(batt1Data.length > 0) {
+
+      // console.log(batt1Data[0].data);
+      // console.log(this.props.data[0].family);
+      // console.log(batt1Data[0].displayName);
+      // console.log(batt1Data[0].unit);
 
       // batt1 = (<RealtimeLine
       //   data={batt1Data[0].data}
@@ -46,8 +48,7 @@ class Motor extends React.Component {
       //   color={this.props.color}
       //   range={{low: 10, high: 14.5}}
       // />)
-
-    }
+  //  }
 
   //  console.log(batt1Data[0].displayName);
 
