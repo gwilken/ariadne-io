65c93e8c914ff8a3fcb1a87ee966c32c9918c7a2
g wilken <greg@gwilken.com>
2017-08-25 19:16:14 -0700

    new family component: motor batteries

--- hub_service/app/components/Main.js
+++ hub_service/app/components/Main.js
@@ -36,32 +36,32 @@ class Main extends React.Component {
 
     var list = this.state.telemetry.map((elem) => { return elem.family; })
 
-    if(list.includes('house')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'house'});
-      house = (
-        <div className="component-container">
-          <House data={data} color="royalblue"/>
-        </div>
-      )
-    }
-
-    if(list.includes('solar')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'solar'});
-      solar = (
-        <div className="component-container">
-          <Solar data={data} color="gold"/>
-        </div>
-      )
-    }
-
-    if(list.includes('motor')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'motor'});
-      motor = (
-        <div className="component-container">
-          <Motor data={data} color="firebrick"/>
-        </div>
-      )
-    }
+    // if(list.includes('house')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'house'});
+    //   house = (
+    //     <div className="component-container">
+    //       <House data={data} color="royalblue"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('solar')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'solar'});
+    //   solar = (
+    //     <div className="component-container">
+    //       <Solar data={data} color="gold"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('motor')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'motor'});
+    //   motor = (
+    //     <div className="component-container">
+    //       <Motor data={data} color="firebrick"/>
+    //     </div>
+    //   )
+    // }
 
     if(list.includes('motorbatt')) {
       var data = this.state.telemetry.filter((elem) => {return elem.family === 'motorbatt'});
@@ -71,24 +71,24 @@ class Main extends React.Component {
         </div>
       )
     }
-
-    if(list.includes('enviro')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'enviro'});
-      enviro = (
-        <div className="component-container">
-          <Enviro data={data} color="darkviolet"/>
-        </div>
-      )
-    }
-
-    if(list.includes('gps')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'gps'});
-      gps = (
-        <div className="component-container">
-          <Gps data={data} color="lightseagreen"/>
-        </div>
-      )
-    }
+    //
+    // if(list.includes('enviro')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'enviro'});
+    //   enviro = (
+    //     <div className="component-container">
+    //       <Enviro data={data} color="darkviolet"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('gps')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'gps'});
+    //   gps = (
+    //     <div className="component-container">
+    //       <Gps data={data} color="lightseagreen"/>
+    //     </div>
+    //   )
+    // }
 
     return (
       <div className="mainContainer">
