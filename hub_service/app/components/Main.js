16bd2727e05031692b2873901c21b6251cb5a843
g wilken <greg@gwilken.com>
2017-08-26 16:41:51 -0700

    building new api route

--- hub_service/app/components/Main.js
+++ hub_service/app/components/Main.js
@@ -12,11 +12,20 @@ class Main extends React.Component {
     super();
 
     this.state = {
+      history: [],
       telemetry: []
     }
   }
 
   componentDidMount() {
+
+    fetch('/all/60')
+      .then((res) => res.json())
+        .then((obj) => {
+          console.log(obj);
+          this.setState({history: obj});
+        });
+
     var ws = new WebSocket('ws://192.168.10.1:8080');
 
     ws.onmessage = function(event) {
@@ -34,61 +43,61 @@ class Main extends React.Component {
         motor,
         motorbatts;
 
-    var list = this.state.telemetry.map((elem) => { return elem.family; })
-
-    if(list.includes('house')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'house'});
-      house = (
-        <div className="component-container">
-          <House data={data} color="royalblue"/>
-        </div>
-      )
-    }
-
-    if(list.includes('solar')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'solar'});
-      solar = (
-        <div className="component-container">
-          <Solar data={data} color="gold"/>
-        </div>
-      )
-    }
-
-    if(list.includes('motor')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'motor'});
-      motor = (
-        <div className="component-container">
-          <Motor data={data} color="firebrick"/>
-        </div>
-      )
-    }
-
-    if(list.includes('motorbatt')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'motorbatt'});
-      motorbatts = (
-        <div className="component-container">
-          <MotorBatteryGroup data={data} color="orange"/>
-        </div>
-      )
-    }
-
-    if(list.includes('enviro')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'enviro'});
-      enviro = (
-        <div className="component-container">
-          <Enviro data={data} color="darkviolet"/>
-        </div>
-      )
-    }
-
-    if(list.includes('gps')) {
-      var data = this.state.telemetry.filter((elem) => {return elem.family === 'gps'});
-      gps = (
-        <div className="component-container">
-          <Gps data={data} color="lightseagreen"/>
-        </div>
-      )
-    }
+    // var list = this.state.telemetry.map((elem) => { return elem.family; })
+    //
+    // if(list.includes('house')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'house'});
+    //   house = (
+    //     <div className="component-container">
+    //       <House data={data} color="royalblue"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('solar')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'solar'});
+    //   solar = (
+    //     <div className="component-container">
+    //       <Solar data={data} color="gold"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('motor')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'motor'});
+    //   motor = (
+    //     <div className="component-container">
+    //       <Motor data={data} color="firebrick"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('motorbatt')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'motorbatt'});
+    //   motorbatts = (
+    //     <div className="component-container">
+    //       <MotorBatteryGroup data={data} color="orange"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('enviro')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'enviro'});
+    //   enviro = (
+    //     <div className="component-container">
+    //       <Enviro data={data} color="darkviolet"/>
+    //     </div>
+    //   )
+    // }
+    //
+    // if(list.includes('gps')) {
+    //   var data = this.state.telemetry.filter((elem) => {return elem.family === 'gps'});
+    //   gps = (
+    //     <div className="component-container">
+    //       <Gps data={data} color="lightseagreen"/>
+    //     </div>
+    //   )
+    // }
 
     return (
       <div className="mainContainer">
