9a0f2194d375a6b11e3f65320ed5cc60e1ea0556
g wilken <greg@gwilken.com>
2017-08-26 10:59:45 -0700

    motor battery group component added

--- hub_service/routes/expressroutes.js
+++ hub_service/routes/expressroutes.js
@@ -69,7 +69,9 @@ router.get('/quicklook/:family/:name/:time', function(req, res) {
 
   mongo.collection.aggregate([
     { $match: { "createdAt": { "$gt": time } } },
+    { $unwind: { "$telemetry"} },
     { $match: { "telemetry.family": req.params.family } }
+
 
     // { $unwind: "$telemetry.data" }
 
