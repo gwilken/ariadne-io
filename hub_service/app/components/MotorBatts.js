e76dd32bf0f65003b8afe47d7b72995d5844fec8
g wilken <greg@gwilken.com>
2017-08-25 21:51:33 -0700

    new mini histories on main page

--- hub_service/app/components/MotorBatts.js
+++ hub_service/app/components/MotorBatts.js
@@ -9,83 +9,45 @@ class MotorBatts extends React.Component {
 
   render() {
 
-    var batt1Comp,
-        batt2Comp,
-        batt3Comp,
-        batt4Comp = null;
-
     var motorBatt1 = this.props.data.filter((elem) => {return elem.displayName === 'Motor Battery 1'});
-
-    if(motorBatt1.length > 0) {
-      batt1Comp = (
-        <RealtimeLine
-          data={motorBatt1[0].data[0].data.toFixed(3)}
-          family={this.props.data[0].family}
-          displayName={motorBatt1[0].data[0].displayName}
-          unit={motorBatt1[0].data[0].unit}
-          color={this.props.color}
-          range={{low: 10, high: 14.5}}
-        />
-      )
-    }
 
     var motorBatt2 = this.props.data.filter((elem) => {return elem.displayName === 'Motor Battery 2'});
 
-    if(motorBatt2.length > 0) {
-      batt2Comp = (
-        <RealtimeLine
-          data={motorBatt2[0].data[0].data.toFixed(3)}
-          family={this.props.data[0].family}
-          displayName={motorBatt2[0].data[0].displayName}
-          unit={motorBatt2[0].data[0].unit}
-          color={this.props.color}
-          range={{low: 10, high: 14.5}}
-        />
-      )
-    }
-
     var motorBatt3 = this.props.data.filter((elem) => {return elem.displayName === 'Motor Battery 3'});
 
-    if(motorBatt3.length > 0) {
-      batt3Comp = (
-        <RealtimeLine
-          data={motorBatt3[0].data[0].data.toFixed(3)}
-          family={this.props.data[0].family}
-          displayName={motorBatt3[0].data[0].displayName}
-          unit={motorBatt3[0].data[0].unit}
-          color={this.props.color}
-          range={{low: 10, high: 14.5}}
-        />
-      )
-    }
-
     var motorBatt4 = this.props.data.filter((elem) => {return elem.displayName === 'Motor Battery 4'});
-
-    if(motorBatt4.length > 0) {
-      batt4Comp = (
-        // <RealtimeLine
-        //   data={motorBatt4[0].data[0].data.toFixed(3)}
-        //   family={this.props.data[0].family}
-        //   displayName={motorBatt4[0].data[0].displayName}
-        //   unit={motorBatt4[0].data[0].unit}
-        //   color={this.props.color}
-        //   range={{low: 10, high: 14.5}}
-        // />
-      )
-    }
 
     return (
       <div>
 
         <h2>Motor Batteries</h2>
 
-          {batt1Comp}
+          <RealtimeLine
+            data={motorBatt1[0].data[0].data.toFixed(3)}
+            family={this.props.data[0].family}
+            displayName={motorBatt1[0].data[0].displayName}
+            unit={motorBatt1[0].data[0].unit}
+            color={this.props.color}
+            range={{low: 10, high: 14.5}}
+          />
 
-          {batt2Comp}
+          <RealtimeLine
+            data={motorBatt2[0].data[0].data.toFixed(3)}
+            family={this.props.data[0].family}
+            displayName={motorBatt2[0].data[0].displayName}
+            unit={motorBatt2[0].data[0].unit}
+            color={this.props.color}
+            range={{low: 10, high: 14.5}}
+          />
 
-          {batt3Comp}
-
-          {/* {batt4Comp} */}
+          <RealtimeLine
+            data={motorBatt3[0].data[0].data.toFixed(3)}
+            family={this.props.data[0].family}
+            displayName={motorBatt3[0].data[0].displayName}
+            unit={motorBatt3[0].data[0].unit}
+            color={this.props.color}
+            range={{low: 10, high: 14.5}}
+          />
 
           <RealtimeLine
             data={motorBatt4[0].data[0].data.toFixed(3)}
