e3d3ff8254f4b193f51a9a6b2ed5e229407150a8
g wilken <greg@gwilken.com>
2017-08-25 17:01:58 -0700

    new family component: motor batteries

--- hub_service/app/components/Main.js
+++ hub_service/app/components/Main.js
@@ -68,7 +68,7 @@ class Main extends React.Component {
       var data = this.state.telemetry.filter((elem) => {return elem.family === 'motorbatt'});
       motorbatts = (
         <div className="component-container">
-          <MotorBatts data={data} color="goldenrod"/>
+          <MotorBatts data={data} color="orange"/>
         </div>
       )
     }
