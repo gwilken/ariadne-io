e3da3c354123e7e52f730e20dca93a55059d5638
g wilken <greg@gwilken.com>
2017-08-26 18:27:51 -0700

    building new api route

--- hub_service/app/components/RealtimeLine.js
+++ hub_service/app/components/RealtimeLine.js
@@ -43,17 +43,15 @@ class RealtimeLine extends React.Component {
 
     var displayDocs = familyDocs.filter((elem) => {
 
-      elem.filter((elem2) => {
+      return(elem.filter((elem2) => {
 
-         elem2.data.filter((elem3) => {
-
-           console.log(elem3.displayName);
+         return (elem2.data.filter((elem3) => {
 
           return elem3.displayName === this.props.displayName;
 
-        });
+        }));
 
-      });
+      }));
 
     })
 
