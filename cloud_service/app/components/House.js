545324bb43125f691506d762dac24ffb3d890871
g wilken <greg@gwilken.com>
2017-08-31 13:42:15 -0700

    better error checking on compononets test

--- cloud_service/app/components/House.js
+++ cloud_service/app/components/House.js
@@ -8,18 +8,60 @@ class House extends React.Component {
     super(props);
   }
 
-  // componentDidMount() {
-  //   console.log(this.props.history());
-  // }
-
   render() {
 
    var house = this.props.data.filter((elem) => {return elem.displayName === 'House'});
-   var houseVoltage = house[0].data.filter((elem) => {return elem.sensor === 'voltage'})
-   var houseCurrent = house[0].data.filter((elem) => {return elem.sensor === 'current'})
+   var volts = null;
+   var current = null;
 
-//   var fridge = this.props.data.filter((elem) => {return elem.displayName === 'Refrigerator'});
-//   var beer = fridge[0].data.filter((elem) => {return elem.displayName === 'Beer Temperature'});
+   var fridge = this.props.data.filter((elem) => {return elem.displayName === 'Refrigerator'});
+   var beer = null;
+
+   if(house.length > 0) {
+     var houseVoltage = house[0].data.filter((elem) => {return elem.sensor === 'voltage'})
+     var houseCurrent = house[0].data.filter((elem) => {return elem.sensor === 'current'})
+
+     var volts = (
+       <RealtimeLine
+        history={this.props.history}
+        data={houseVoltage[0].data.toFixed(2)}
+        family={this.props.data[0].family}
+        displayName={houseVoltage[0].displayName}
+        unit={houseVoltage[0].unit}
+        color={this.props.color}
+        range={{low: 10, high: 14.5}}
+      />
+     )
+
+     var current = (
+       <RealtimeLine
+        history={this.props.history}
+        data={houseCurrent[0].data.toFixed(2)}
+        family={this.props.data[0].family}
+        displayName={houseCurrent[0].displayName}
+        unit={houseCurrent[0].unit}
+        color={this.props.color}
+        range={{low: 0, high: 7500}}
+       />
+     )
+   }
+
+   if(fridge.length > 0) {
+    var data = fridge[0].data.filter((elem) => {return elem.displayName === 'Beer Temperature'});
+    var displayName = data[0].displayName;
+
+    beer = (
+      <RealtimeLine
+       history={this.props.history}
+       data={data[0].data.toFixed(2)}
+       family={this.props.data[0].family}
+       displayName={displayName}
+       unit={'F'}
+       color={this.props.color}
+       range={{low: 32, high: 100}}
+      />
+    )
+  }
 
     return (
 
@@ -29,35 +71,9 @@ class House extends React.Component {
 
         <div className="family-container">
 
-           <RealtimeLine
-            history={this.props.history}
-            data={houseVoltage[0].data.toFixed(2)}
-            family={this.props.data[0].family}
-            displayName={houseVoltage[0].displayName}
-            unit={houseVoltage[0].unit}
-            color={this.props.color}
-            range={{low: 10, high: 14.5}}
-          />
-
-          <RealtimeLine
-           history={this.props.history}
-           data={houseCurrent[0].data.toFixed(2)}
-           family={this.props.data[0].family}
-           displayName={houseCurrent[0].displayName}
-           unit={houseCurrent[0].unit}
-           color={this.props.color}
-           range={{low: 0, high: 7500}}
-          />
-
-          {/* <RealtimeLine
-           history={this.props.history}
-           data={beer[0].data.toFixed(2)}
-           family={this.props.data[0].family}
-           displayName={beer[0].displayName}
-           unit={'F'}
-           color={this.props.color}
-           range={{low: 32, high: 100}}
-          /> */}
+          {volts}
+          {current}
+          {beer}
 
         </div>
 
