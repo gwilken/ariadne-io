9de6256cfcf6cea9fc54a2fd826f26bc9b83d485
g wilken <greg@gwilken.com>
2017-09-01 13:08:24 -0700

    rewriting functional data organization scheme

--- hub_service/app/components/Main.js
+++ hub_service/app/components/Main.js
@@ -36,33 +36,34 @@ class Main extends React.Component {
   }
 
   didLoad(obj) {
+    console.log(obj);
     this.setState({history: obj});
   }
 
   getHistory(family, name) {
-    var filterDocs = [];
-
-    var familyDocs = this.state.history.map((elem) => {
-      return elem.filter((elem2) => {
-          return elem2.family === family;
-        })
-    })
-
-    familyDocs.forEach((elem) => {
-      elem.forEach((elem2) => {
-        elem2.data.forEach((elem3) => {
-          if(elem3.displayName === name) {
-            filterDocs.push(elem3)
-          }
-        });
-      });
-    })
-
-    var res = filterDocs.map((elem) => {
-      return elem.data;
-    })
-
-    return(res);
+    // var filterDocs = [];
+    //
+    // var familyDocs = this.state.history.map((elem) => {
+    //   return elem.filter((elem2) => {
+    //       return elem2.family === family;
+    //     })
+    // })
+    //
+    // familyDocs.forEach((elem) => {
+    //   elem.forEach((elem2) => {
+    //     elem2.data.forEach((elem3) => {
+    //       if(elem3.displayName === name) {
+    //         filterDocs.push(elem3)
+    //       }
+    //     });
+    //   });
+    // })
+    //
+    // var res = filterDocs.map((elem) => {
+    //   return elem.data;
+    // })
+    //
+    // return(res);
   }
 
   render() {
