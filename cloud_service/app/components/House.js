6887313908b760465464e06bb00f4608f7cc4e27
g wilken <greg@gwilken.com>
2017-09-01 19:47:02 -0700

    rewriting functional data for components

--- cloud_service/app/components/House.js
+++ cloud_service/app/components/House.js
@@ -10,74 +10,59 @@ class House extends React.Component {
 
   render() {
 
-   var house = this.props.data.filter((elem) => {return elem.displayName === 'House'});
-   var volts = null;
-   var current = null;
+   var house =
+      this.props.data.filter((elem) => elem.displayName === 'House')
+      .reduce((acc, val) => val.concat(acc))
+      .data
+      .map((item) => {
 
-   var fridge = this.props.data.filter((elem) => {return elem.displayName === 'Refrigerator'});
-   var beer = null;
+        return (
+          <RealtimeLine
+           history={this.props.history}
+           data={item.data.toFixed(2)}
+           family={this.props.data[0].family}
+           displayName={item.displayName}
+           unit={item.unit}
+           color={this.props.color}
+         />
+        )
 
-   if(house.length > 0) {
-     var houseVoltage = house[0].data.filter((elem) => {return elem.sensor === 'voltage'})
-     var houseCurrent = house[0].data.filter((elem) => {return elem.sensor === 'current'})
+      });
 
-     var volts = (
-       <RealtimeLine
-        history={this.props.history}
-        data={houseVoltage[0].data.toFixed(2)}
-        family={this.props.data[0].family}
-        displayName={houseVoltage[0].displayName}
-        unit={houseVoltage[0].unit}
-        color={this.props.color}
-        range={{low: 10, high: 14.5}}
-      />
-     )
+    var beer =
+       this.props.data.filter((elem) => elem.displayName === 'Refrigerator')
+       .reduce((acc, val) => val.concat(acc))
+       .data
+       .map((item) => {
 
-     var current = (
-       <RealtimeLine
-        history={this.props.history}
-        data={houseCurrent[0].data.toFixed(2)}
-        family={this.props.data[0].family}
-        displayName={houseCurrent[0].displayName}
-        unit={houseCurrent[0].unit}
-        color={this.props.color}
-        range={{low: 0, high: 7500}}
-       />
-     )
-   }
+         return (
+           <RealtimeLine
+            history={this.props.history}
+            data={item.data.toFixed(2)}
+            family={this.props.data[0].family}
+            displayName={item.displayName}
+            unit={item.unit}
+            color={this.props.color}
+            range={{low: 10, high: 14.5}}
+          />
+         )
 
-   if(fridge.length > 0) {
-    var data = fridge[0].data.filter((elem) => {return elem.displayName === 'Beer Temperature'});
-    var displayName = data[0].displayName;
+       });
 
-    beer = (
-      <RealtimeLine
-       history={this.props.history}
-       data={data[0].data.toFixed(2)}
-       family={this.props.data[0].family}
-       displayName={displayName}
-       unit={'F'}
-       color={this.props.color}
-       range={{low: 32, high: 100}}
-      />
-    )
-  }
 
     return (
 
       <div>
-
         <h2>House</h2>
 
         <div className="family-container">
 
-          {volts}
-          {current}
+          {house}
           {beer}
 
         </div>
-
       </div>
+
     )
   }
 }
--- cloud_service/app/components/Motor.js
+++ cloud_service/app/components/Motor.js
@@ -13,72 +13,36 @@ class Motor extends React.Component {
 
   render() {
 
-    var motorData = this.props.data.filter((elem) => {return elem.displayName === 'Electric Yacht 10kW Motor'});
+    var motor =
+       this.props.data.filter((elem) => elem.displayName === 'Electric Yacht 10kW Motor')
+       .reduce((acc, val) => val.concat(acc))
+       .data
+       .map((item) => {
 
-    if(motorData.length > 0) {
-      var bankVoltage = motorData[0].data.filter((elem) => {return elem.sensor === "volts"});
-      var motorSOC = motorData[0].data.filter((elem) => {return elem.sensor === "soc"});
-      var motorCurrent = motorData[0].data.filter((elem) => {return elem.sensor === "current"});
-      var motorTTD = motorData[0].data.filter((elem) => {return elem.sensor === "ttd"});
-      var motorRPM = motorData[0].data.filter((elem) => {return elem.sensor === "rpm"});
-    }
+         return (
+           <RealtimeLine
+            history={this.props.history}
+            data={item.data.toFixed(2)}
+            family={this.props.data[0].family}
+            displayName={item.displayName}
+            unit={item.unit}
+            color={this.props.color}
+          />
+         )
+
+       });
+
 
     return (
 
       <div>
-
         <h2>Electric Motor</h2>
 
-          <RealtimeLine
-            history={this.props.history}
-            data={bankVoltage[0].data}
-            family={this.props.data[0].family}
-            displayName={this.props.data[0].data[3].displayName}
-            unit={this.props.data[0].data[3].unit}
-            color={this.props.color}
-            range={{low: 46, high: 58}}
-          />
+          <div className="family-container">
 
-          <RealtimeLine
-            history={this.props.history}
-            data={motorCurrent[0].data}
-            family={this.props.data[0].family}
-            displayName={this.props.data[0].data[2].displayName}
-            unit={this.props.data[0].data[2].unit}
-            color={this.props.color}
-            range={{low: 0, high: 10}}
-          />
+            {motor}
 
-          <RealtimeLine
-            history={this.props.history}
-            data={motorTTD[0].data}
-            family={this.props.data[0].family}
-            displayName={this.props.data[0].data[1].displayName}
-            unit={this.props.data[0].data[1].unit}
-            color={this.props.color}
-            range={{low: 0, high: 20}}
-          />
-
-          <RealtimeLine
-            history={this.props.history}
-            data={motorSOC[0].data.mapRange(0,255,0,100)}
-            family={this.props.data[0].family}
-            displayName={this.props.data[0].data[0].displayName}
-            unit={this.props.data[0].data[0].unit}
-            color={this.props.color}
-            range={{low: 0, high: 100}}
-          />
-
-          <RealtimeLine
-            history={this.props.history}
-            data={motorRPM[0].data}
-            family={this.props.data[0].family}
-            displayName={this.props.data[0].data[4].displayName}
-            unit={this.props.data[0].data[4].unit}
-            color={this.props.color}
-            range={{low: 0, high: 2000}}
-          />
-
+        </div>
       </div>
       )
   }
