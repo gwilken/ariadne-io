60a19b1fb1c20d7d967b3190b0e2a859161c76a6
Greg Wilken <greg@gwilken.com>
2017-12-28 21:03:14 -0800

    webpacked

--- hub_service/webpack.config.js
+++ hub_service/webpack.config.js
@@ -53,6 +53,6 @@ module.exports = {
   ],
   // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
   // Without this the console says all errors are coming from just coming from bundle.js
-  devtool: "cheap-module-source-map"
-  //devtool: "eval-source-map"
+  //devtool: "cheap-module-source-map"
+  devtool: "eval-source-map"
 };
